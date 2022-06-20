c2dedf709b6e33a7b60ba5dcdcfc3b3f276c20e7
masa002 <m.obara.sys21@morijyobi.ac.jp>
2022-06-20 17:14:25 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # RealTimeTranslate
- 
-How to install
--- Translate.py
+++ Translate.py
@@ -1,25 +1,39 @@
+import subprocess
+import webbrowser
 import configparser
+import sys
 
-import pyttsx3
 import speech_recognition as sr
+import pyaudio
 import deepl
 
-def Say(data):
-    s.say(data)  
-    s.runAndWait()
+def Say(text):
+    try:
+        _start = "start ./softalk/softalk.exe"
+        _speed = "/S:" + config.get('TALK_SETTINGS', 'speed')
+        _hide = "/X:1" if config.getboolean('TALK_SETTINGS', 'hide') else ""
+        _word = "/W:" + text
+        _command = [_start, _speed, _hide, _word]
+
+        subprocess.run(' '.join(_command), shell=True)
+    
+    except:
+        print("Softalk Error")
+        return
 
 def Translate(text):
     try:
         source_lang = lang[0]
         target_lang = lang[1]
-        translator = deepl.Translator(config.get('API_KEY', 'key'))
+        translator = deepl.Translator(key)
 
-        result = translator.translate_text(text, target_lang="en-US").text
+        result = translator.translate_text(text, target_lang=target_lang).text
 
-        Say(result)
-        
     except:
+        print("Translation Error")
         return
+
+    Say(result)
 
 def main():
     language = lang[0]
@@ -35,10 +49,11 @@ def main():
         print ("Now to recognize it...")
         try:
             text = r.recognize_google(audio, language=language)
-            print(lang[mode] + " : " + text)
+            print(language + " : " + text)
 
             # ストップといって終了
             if text == "ストップ" or text == "stop":
+                subprocess.run("start ./softalk/softalk.exe /close_now", shell=True)
                 print("end")
                 break
 
@@ -54,15 +69,24 @@ def main():
 
 
 if __name__ == '__main__':
-    s = pyttsx3.init()
-    r = sr.Recognizer()
-    mic = sr.Microphone()
-    lang = []
+    try:
+        r = sr.Recognizer()
+        mic = sr.Microphone()
+        lang = []
 
-    config = configparser.ConfigParser()
-    config.read('./config.ini')
+        config = configparser.ConfigParser()
+        config.read('./config.ini')
 
-    lang.append(config.get('TRANSLATE_SETTINGS', 'source_lang'))
-    lang.append(config.get('TRANSLATE_SETTINGS', 'target_lang'))
+        key = config.get('API_KEY', 'key')
+        if key == "":
+            print("Please Enter the API key for deepl in the config.ini")
+            webbrowser.open('https://www.deepl.com/pro-api?cta=header-pro-api', autoraise=True)
+            sys.exit(0)
+
+        lang.append(config.get('TRANSLATE_SETTINGS', 'source_lang'))
+        lang.append(config.get('TRANSLATE_SETTINGS', 'target_lang'))
+    
+    except:
+        print("Loading Error")
 
     main()
